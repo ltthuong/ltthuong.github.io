c27d84cd9be898f1800c6f24845e175d0d5e6bb9
Thuong Le <letienthuong.vn@gmail.com>
2020-03-10 13:38:36 +0700

    .gitignore fix

---
+++ build/precache-manifest.bad4633856bf20a7959cb9bd578f2d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b25cfd52b67d83dc2e2f806c1c86fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6829089dd506874c45c0",
+    "url": "/static/css/main.178bbd59.chunk.css"
+  },
+  {
+    "revision": "a5c51d5564ece4bc6e34",
+    "url": "/static/js/2.6d217a95.chunk.js"
+  },
+  {
+    "revision": "f1a03c1f2e879f3cba3e2b57eabb23eb",
+    "url": "/static/js/2.6d217a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6829089dd506874c45c0",
+    "url": "/static/js/main.9eaebbb2.chunk.js"
+  },
+  {
+    "revision": "d88b58b71d27b2aa809e",
+    "url": "/static/js/runtime-main.f732b77c.js"
+  }
+]);
