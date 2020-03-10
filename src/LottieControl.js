c1efe0a8eadff74ea6fb830bb4ec60a151c3465d
Thuong Le <letienthuong.vn@gmail.com>
2020-03-10 11:59:59 +0700

    Add lottie

---
+++ src/LottieControl.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import * as animationData from './json/smartsharp-animations.json';
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
+class LottieControl extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Lottie options={defaultOptions} height={window.innerHeight}/>
+      </div>
+    );
+  }
+}
+
+export default LottieControl;
